89a1f7f819d898b38aa746f803b6400f8c072222
john lester escarlan <jlescarlan11@gmail.com>
2025-07-25 23:03:06 +0800

    INIT_API

---
+++ src/main/java/com/codewithmosh/store/entities/Message.java
@@ -0,0 +1,20 @@
+package com.codewithmosh.store.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Data
+@Setter
+@EqualsAndHashCode
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+    private String text;
+}
