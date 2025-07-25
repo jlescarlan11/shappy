fba861105bbfa8232d6ebb4432ba219f97a6f11c
john lester escarlan <jlescarlan11@gmail.com>
2025-07-26 00:44:09 +0800

    Extract Query PArameters

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -1,9 +1,13 @@
 package com.codewithmosh.store.controllers;
 
+import java.util.Set;
+
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codewithmosh.store.dtos.UserDto;
@@ -21,8 +25,12 @@ public class UserController {
     private final UserMapper userMapper;
 
     @GetMapping
-    public Iterable<UserDto> getAllUsers() {
-        return userRepository.findAll().stream().map(userMapper::toDto).toList();
+    public Iterable<UserDto> getAllUsers(
+        @RequestParam(required=false, defaultValue="") String sort
+    ) {
+        if (!Set.of("name","email").contains(sort))
+            sort = "name";
+        return userRepository.findAll(Sort.by(sort)).stream().map(userMapper::toDto).toList();
     }
 
     @GetMapping("/{id}")
